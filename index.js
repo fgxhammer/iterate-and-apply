23dea7031f6ec57b8be81b15c1d9fbf0f8ffe19d
Franz Gollhammer <franz.gollhammer@gmail.com>
2021-10-12 14:42:31 +0200

    add type check and return new copy

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-module.exports =  (obj, fun) => {
+module.exports =  (input, fun) => {
+  if (typeof input !== 'object') {
+    const x = input
+    return fun(x)
+  }
+
+  const obj = JSON.parse(JSON.stringify(input))
   for (const i in obj) {
     if (typeof obj[i] === 'object' && obj[i] !== null) {
       iterateRecursiveAndApplyFun(obj[i])
@@ -8,4 +14,5 @@ module.exports =  (obj, fun) => {
       }
     }
   }
+  return obj
 }
